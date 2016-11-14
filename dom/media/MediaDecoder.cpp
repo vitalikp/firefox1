04c995d1a684129c72a0fa214b100ec0b2ae9ea9
JW Wang <jwwang@mozilla.com>
2016-11-14 16:47:07 +0800

    Bug 1317576. Part 3 - remove unused parameter/member. r=cpearce
    MozReview-Commit-ID: Ff6a8MKVY8q

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1164,15 +1164,10 @@ MediaDecoder::OnSeekResolved(SeekResolveValue aVal)
   MOZ_ASSERT(!IsShutdown());
   mSeekRequest.Complete();
 
-  bool fireEnded = false;
   {
     // An additional seek was requested while the current seek was
     // in operation.
     UnpinForSeek();
-    fireEnded = aVal.mAtEnd;
-    if (aVal.mAtEnd) {
-      ChangeState(PLAY_STATE_ENDED);
-    }
     mLogicallySeeking = false;
   }
 
@@ -1181,9 +1176,6 @@ MediaDecoder::OnSeekResolved(SeekResolveValue aVal)
 
   mOwner->SeekCompleted();
   AsyncResolveSeekDOMPromiseIfExists();
-  if (fireEnded) {
-    mOwner->PlaybackEnded();
-  }
 }
 
 void
