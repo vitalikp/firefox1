3318bb7c15b5053fb0087ee67d8d85c10703ccd1
Sylvestre Ledru <sledru@mozilla.com>
2017-02-09 15:31:35 +0100

    Bug 1338086 - Remove useless else blocks in order to reduce complexity in xpcom/ r=froydnj
    MozReview-Commit-ID: JUJARn5Nspp

--- xpcom/build/FileLocation.cpp
+++ xpcom/build/FileLocation.cpp
@@ -186,7 +186,7 @@ FileLocation::Data::GetSize(uint32_t* aResult)
     *aResult = fileInfo.size;
     return NS_OK;
   }
-  else if (mItem) {
+  if (mItem) {
     *aResult = mItem->RealSize();
     return NS_OK;
   }
@@ -207,7 +207,7 @@ FileLocation::Data::Copy(char* aBuf, uint32_t aLen)
     }
     return NS_OK;
   }
-  else if (mItem) {
+  if (mItem) {
     nsZipCursor cursor(mItem, mZip, reinterpret_cast<uint8_t*>(aBuf),
                        aLen, true);
     uint32_t readLen;
