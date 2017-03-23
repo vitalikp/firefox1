5046fe05b7743be751dd4c802c69c791f691909c
Jeremy Chen <jeremychen@mozilla.com>
2017-03-23 21:59:55 +0800

    Bug 1347819 - change nsFont::languageOverride to store uint32_t directly. r=jfkthame
    Since font-language-override can only have a single three-letter tag, and it is
    eventually converted to uint32_t while creating gfxFontStyle, we should be able
    to move the conversion ahead, to an earlier stage.
    
    In this patch, we move the ParseFontLanguageOverride to nsRuleNode, so we could
    do the nsString-to-uint32_t conversion during computing time.
    
    MozReview-Commit-ID: LA4Bv3wV7K

--- layout/style/nsRuleNode.h
+++ layout/style/nsRuleNode.h
@@ -1045,6 +1045,8 @@ public:
                                         nsPresContext* aPresContext,
                                         nsFontSizeType aFontSizeType = eFontSize_HTML);
 
+  static uint32_t ParseFontLanguageOverride(const nsAString& aLangTag);
+
   /**
    * @param aValue The color value, returned from nsCSSParser::ParseColorString
    * @param aPresContext Presentation context whose preferences are used
