aca3f96e5326672c12c7dfcd3e17dcd4f4a69f82
Kris Maglione <maglione.k@gmail.com>
2017-08-02 13:51:40 -0700

    Bug 1350646: Part 1 - Remove addon-sdk moz.build generation gunk. r=Mossop
    MozReview-Commit-ID: GMlOFoosVQZ

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -95,7 +95,6 @@ SEARCH_PATHS = [
 
 # Individual files providing mach commands.
 MACH_MODULES = [
-    'addon-sdk/mach_commands.py',
     'build/valgrind/mach_commands.py',
     'devtools/shared/css/generated/mach_commands.py',
     'dom/bindings/mach_commands.py',
