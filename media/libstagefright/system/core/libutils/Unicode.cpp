00052a642ae33ee970f2404b4107eb91d1164e17
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2019-03-19 01:23:14 +0200

    * media: fix fall through warning;

--- media/libstagefright/system/core/libutils/Unicode.cpp
+++ media/libstagefright/system/core/libutils/Unicode.cpp
@@ -108,8 +108,11 @@ static inline void utf32_codepoint_to_utf8(uint8_t* dstP, char32_t srcChar, size
     switch (bytes)
     {   /* note: everything falls through. */
         case 4: *--dstP = (uint8_t)((srcChar | kByteMark) & kByteMask); srcChar >>= 6;
+            // fallthrough
         case 3: *--dstP = (uint8_t)((srcChar | kByteMark) & kByteMask); srcChar >>= 6;
+            // fallthrough
         case 2: *--dstP = (uint8_t)((srcChar | kByteMark) & kByteMask); srcChar >>= 6;
+            // fallthrough
         case 1: *--dstP = (uint8_t)(srcChar | kFirstByteMark[bytes]);
     }
 }
